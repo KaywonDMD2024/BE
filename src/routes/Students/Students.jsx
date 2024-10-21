a9c4afa323ff5bd96cae3acf3fe21b08fde5571c
jaerim <jaerim1102@naver.com>
2024-10-21 21:02:25 +0900

    프로필 컴포넌트

--- src/routes/Students/Students.jsx
+++ src/routes/Students/Students.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StudentsHeader from "./components/StudentsHeader";
+import ProfileComponent from "./components/ProfileComponent";
 
 const Students = () => {
   return (
@@ -7,6 +8,7 @@ const Students = () => {
       <StudentsHeader />
       <h1>클리커 게임</h1>
       <p>이곳에서 클리커 게임을 플레이하세요.</p>
+      <ProfileComponent />
     </div>
   );
 };
