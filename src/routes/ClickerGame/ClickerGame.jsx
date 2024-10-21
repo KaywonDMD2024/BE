48b0ec769f1cea01722e98640cb086b58af5ebd6
Soeun Ryu <soeunl131@gmail.com>
2024-10-21 13:54:11 +0900

    css 추가

--- src/routes/ClickerGame/ClickerGame.jsx
+++ src/routes/ClickerGame/ClickerGame.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "./style.css"
+// import "../styles/pretendard.css";
 import imgCharacter1 from "./assets/character-1.svg";
 import imgCharacter2 from "./assets/character-2.svg";
 import imgCharacter3 from "./assets/character-3.svg";
@@ -15,26 +17,28 @@ const ClickerGame = () => {
 
   return (
     <div>
-      <h1>
-        {clickCount >= 30
-          ? "Growth is complete!"
-          : clickCount >= 10
-          ? "Need more click..."
-          : "Click Me!"
-        }
-      </h1>
-      <img
-        src={
-          clickCount >= 30 ? imgCharacter3
-            : clickCount >= 10 ? imgCharacter2
-            : imgCharacter1
-        }
-        alt="캐릭터"
-        className="character"
-        onClick={handleClick} // 클릭 이벤트 핸들러
-        style={{ cursor: "pointer" }} // 커서 스타일 추가
-      />
-      <p>Clicks: {clickCount}</p>
+      <div className="contents">
+        <h1>
+          {clickCount >= 30
+            ? "Growth is complete!"
+            : clickCount >= 10
+            ? "Need more click..."
+            : "Click Me!"
+          }
+        </h1>
+        <img
+          src={
+            clickCount >= 30 ? imgCharacter3
+              : clickCount >= 10 ? imgCharacter2
+              : imgCharacter1
+          }
+          alt="캐릭터"
+          className="character"
+          onClick={handleClick} // 클릭 이벤트 핸들러
+          style={{ cursor: "pointer" }} // 커서 스타일 추가
+        />
+        <p className="clickNum">Clicks: {clickCount}</p>
+      </div>
     </div>
   );
 };
