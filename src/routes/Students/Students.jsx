07001637266055ad3ab4468d25a40bcab55e730d
jaerim <jaerim1102@naver.com>
2024-10-21 18:04:18 +0900

    학생 페이지 헤더 마크업

--- src/routes/Students/Students.jsx
+++ src/routes/Students/Students.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import StudentsHeader from "./components/StudentsHeader";
 
 const Students = () => {
   return (
     <div>
+      <StudentsHeader />
       <h1>클리커 게임</h1>
       <p>이곳에서 클리커 게임을 플레이하세요.</p>
     </div>
